96e1f4185da3020380f740b831daf0e115f72905
androidpirate <emrekalaycidesign@gmail.com>
2018-06-12 12:31:55 -0400

    feat - Add click listeners for each card and implementation

--- js/app.js
+++ js/app.js
@@ -72,3 +72,17 @@ function match(e1, e2) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ var cardList = Array.from(deckElement.childNodes);
+ cardList.forEach(function(card){
+   card.addEventListener("click", function(element){
+     if(openCards.length <= 1) {
+       element.target.className = "card match";
+       openCards.push(card);
+       increaseMoves();
+     }
+     // Check if the cards match
+     setTimeout(function(){
+       match(openCards[0], openCards[1]);
+     }, 1000);
+   });
+ });
