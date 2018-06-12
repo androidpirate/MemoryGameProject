895fd8c3025e0c63b87d05e571587e0818b3a6e6
androidpirate <emrekalaycidesign@gmail.com>
2018-06-12 12:09:53 -0400

    feat - Add a list that holds all the cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 /*
  * Create a list that holds all of your cards
  */
-
+ const deck = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o",
+               "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube",
+               "fa fa-anchor","fa fa-anchor", "fa fa-leaf", "fa fa-leaf",
+               "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 
 /*
  * Display the cards on the page
