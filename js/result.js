296800312707c908aeaeb2a09469d5851d4e2e72
android_pirate <emrekalaycidesign@gmail.com>
2018-06-16 21:47:24 -0400

    refactor - Format total time to display two decimal points

--- js/result.js
+++ js/result.js
@@ -1,5 +1,6 @@
 const moveCounterElement = document.getElementById("move-counter-element");
 const restartButton = document.getElementById("restart-button");
+const totalTimeElement = document.getElementById("total-time-element");
 
 var moveCount = localStorage.getItem("moveCount");
 var totalTime = localStorage.getItem("totalTime");
@@ -8,12 +9,18 @@ initialize();
 
 function initialize() {
   setMoveCounter();
+  setTotalTime();
 }
 
 restartButton.addEventListener("click", function(element){
   window.location.href = "index.html";
 });
 
+// Set move count
 function setMoveCounter() {
   moveCounterElement.textContent = moveCount;
+}
+
+function setTotalTime() {
+  totalTimeElement.textContent = Number(totalTime).toFixed(2);
 }
